36f265c5a7c5956382b226fa7cbb7b0124a4dd13
kyraha@gmail.com <kyraha@gmail.com@5c57491c-2248-0410-9110-73031f465853>
2013-01-10 06:02:34 +0000

    Declared the Session class

--- trunk/yakutia/index.cpp
+++ trunk/yakutia/index.cpp
@@ -10,43 +10,63 @@
 using namespace std;
 using namespace cgicc;
 
+class Session : public Cgicc {
+	public:
+		static const char* COOKIE_id() {return "cuid";};
+		string	id;
+		HTTPContentHeader* header;
+
+		Session();
+		virtual ~Session();
+
+};
+
+Session::~Session()
+{
+	delete header;
+}
+
+Session::Session() : Cgicc(), header(new HTTPHTMLHeader)
+{
+	const CgiEnvironment& env = getEnvironment();
+	// Iterate through the vector, and print out each value
+	const_cookie_iterator iter;
+	for(iter = env.getCookieList().begin(); 
+		iter != env.getCookieList().end(); 
+		++iter) {
+		if( iter->getName() == COOKIE_id() )
+			id = iter->getValue();
+	}
+
+	if(id.empty()) {
+		uuid guid;
+		guid.make(UUID_MAKE_V1);
+		id = guid.string();
+	}
+
+}
+
 int main(int argc, char**argv)
 {
 	try {
-		Cgicc cgi;
-		const CgiEnvironment& env = cgi.getEnvironment();
-		// Iterate through the vector, and print out each value
-		const_cookie_iterator iter;
-		string cuid;
-		for(iter = env.getCookieList().begin(); 
-			iter != env.getCookieList().end(); 
-			++iter) {
-			if( iter->getName() == "cuid" )
-				cuid = iter->getValue();
-		}
+		Session sess;
 
-		if(cuid.empty()) {
-			uuid id;
-			id.make(UUID_MAKE_V1);
-			cuid = id.string();
-		}
-
-		cout << HTTPHTMLHeader()
-			.setCookie(HTTPCookie("cuid", cuid))
+		cout << sess.header
+			->setCookie(HTTPCookie(Session::COOKIE_id(), sess.id))
 			<< endl;
 
 		// Set up the HTML document
-		cout << html() << head(title("cgicc example")) << endl;
+		cout << html() << head(title("example")) << endl;
 		cout << body() << endl;
 
 		cout << h1("Testing...");
 
-		if( !cuid.empty() )
-			cout << "Cookie cuid received as: " << cuid << br() << endl;
+		if( !sess.id.empty() )
+			cout << "Cookie received is: " << sess.id << br() << endl;
 
 		// Print out the submitted element
-		form_iterator name = cgi.getElement("name");
-		if(name != cgi.getElements().end()) {
+		form_iterator name = sess.getElement("name");
+		if(name != sess.getElements().end()) {
 			cout << "Your name: " << **name << endl;
 		}
 
