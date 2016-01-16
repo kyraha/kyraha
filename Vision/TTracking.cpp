f6e41fe1b6b9544545f30c611ef709820f8c128a
Mikhail Kyraha <mikhail@kyraha.com>
2016-01-15 23:56:33 -0600

    Vision: Restructure the code
    Not much of functionality change, just subroutines and using different
    std:: functions and such.

--- Vision/TTracking.cpp
+++ Vision/TTracking.cpp
@@ -5,6 +5,87 @@
 
 #include <iostream>
 #include <list>
+#include <algorithm>
+
+
+class DataSet : public std::list < cv::Vec3d > {
+public:
+	cv::Vec3d GetMedian();
+};
+
+cv::Vec3d DataSet::GetMedian()
+{
+	if (size() > 2) {
+		std::vector<double> ordX, ordY, ordZ;
+		for (cv::Vec3d dp : *this) {
+			ordX.push_back(dp[0]);
+			ordY.push_back(dp[1]);
+			ordZ.push_back(dp[2]);
+		}
+		std::sort(ordX.begin(), ordX.end());
+		std::sort(ordY.begin(), ordY.end());
+		std::sort(ordZ.begin(), ordZ.end());
+		return cv::Vec3d(ordX[ordX.size() / 2], ordY[ordY.size() / 2], ordZ[ordZ.size() / 2]);
+	}
+	else if (size() > 0) return *(this->begin());
+	else return cv::Vec3d(0, 0, 0);
+}
+
+std::vector<cv::Point3d> objectPoints;
+
+
+cv::Vec3d CalculateLocation(std::vector<cv::Point> target)
+{
+	/* Logitech camera, millimeters
+	cv::Matx33d cameraMatrix(
+	4.9410254557831900e+002, 0, 3.7487505597946938e+002,
+	0, 4.9180258750779660e+002, 2.0903256450552996e+002,
+	0, 0, 1);
+	cv::Matx<double, 5, 1> distCoeffs(
+	-2.0994774118936483e-002,
+	4.9899418109244344e-002,
+	-6.7418809421477795e-004,
+	-5.2140053606489992e-004,
+	-6.9063255104469826e-002);
+	*/
+
+	/* Microsoft HD3000 camera, inches */
+	cv::Matx33d camera_matrix(
+		7.4230925920305481e+002, 0., 3.0383585011521706e+002, 0.,
+		7.4431328863404576e+002, 2.3422929172706634e+002, 0., 0., 1.);
+	cv::Matx<double, 5, 1> distortion_coefficients(
+		2.0963551753568421e-001, -1.4796846132520820e+000, 0., 0., 2.7677879392937270e+000);
+
+	//Extract 4 corner points assuming the blob is a rectanle, more or less horizontal
+	std::vector<cv::Point> hull(4);
+	hull[0] = cv::Point(10000, 10000);		// North-West
+	hull[1] = cv::Point(0, 10000);			// North-East
+	hull[2] = cv::Point(0, 0);				// South-East
+	hull[3] = cv::Point(10000, 0);			// South-West
+	for (cv::Point point : target) {
+		if (hull[0].x + hull[0].y > point.x + point.y) hull[0] = point;
+		if (hull[1].y - hull[1].x > point.y - point.x) hull[1] = point;
+		if (hull[2].x + hull[2].y < point.x + point.y) hull[2] = point;
+		if (hull[3].x - hull[3].y > point.x - point.y) hull[3] = point;
+	}
+
+	//cv::polylines(Im, hull, true, cv::Scalar(0, 255, 0));
+
+	// Make 'em double
+	std::vector<cv::Point2d> imagePoints(4);
+	imagePoints[0] = hull[0];
+	imagePoints[1] = hull[1];
+	imagePoints[2] = hull[2];
+	imagePoints[3] = hull[3];
+
+	cv::Vec3d rvec, tvec;
+	cv::solvePnP(objectPoints, imagePoints, camera_matrix, distortion_coefficients, rvec, tvec, false, CV_EPNP);
+
+	cv::Matx33d Rmat;
+	cv::Rodrigues(rvec, Rmat);
+
+	return -(Rmat.t() * tvec);
+}
 
 int main(int argc, char** argv)
 {
@@ -22,20 +103,21 @@ int main(int argc, char** argv)
 	cv::Mat BlobIm;
 	cv::namedWindow("Image", CV_WINDOW_AUTOSIZE);
 	cv::namedWindow("Blob", CV_WINDOW_AUTOSIZE);
-//	cv::namedWindow("Test", CV_WINDOW_AUTOSIZE);
 
-	cv::createTrackbar("H. Lower", "Blob", &(BlobLower[0]), 255);
-	cv::createTrackbar("H. Upper", "Blob", &(BlobUpper[0]), 255);
-	cv::createTrackbar("S. Lower", "Blob", &(BlobLower[1]), 255);
-	cv::createTrackbar("S. Upper", "Blob", &(BlobUpper[1]), 255);
-	cv::createTrackbar("V. Lower", "Blob", &(BlobLower[2]), 255);
-	cv::createTrackbar("V. Upper", "Blob", &(BlobUpper[2]), 255);
-
-	std::vector<cv::Point3d> objectPoints;
-	objectPoints.push_back(cv::Point3d(-10,  0, 0));
-	objectPoints.push_back(cv::Point3d( 10,  0, 0));
-	objectPoints.push_back(cv::Point3d( 10, 14, 0));
+	/* Real FIRSTSTRONGHOLD tower, inches */
+	objectPoints.push_back(cv::Point3d(-10, 0, 0));
+	objectPoints.push_back(cv::Point3d(10, 0, 0));
+	objectPoints.push_back(cv::Point3d(10, 14, 0));
 	objectPoints.push_back(cv::Point3d(-10, 14, 0));
+
+	/* Whiteboard res target, millimeters
+	objectPoints.push_back(cv::Point3d(-135, 0, 0));
+	objectPoints.push_back(cv::Point3d(135, 0, 0));
+	objectPoints.push_back(cv::Point3d(135, 150, 0));
+	objectPoints.push_back(cv::Point3d(-135, 150, 0));
+	*/
+
+
 
 	std::vector<cv::Point> stencil;
 	stencil.push_back(cv::Point(  9,   0));
@@ -47,16 +129,20 @@ int main(int argc, char** argv)
 	stencil.push_back(cv::Point(212, 100));
 	stencil.push_back(cv::Point(  0, 100));
 
-	cv::Matx33d camera_matrix(
-		7.4230925920305481e+002, 0., 3.0383585011521706e+002, 0.,
-		7.4431328863404576e+002, 2.3422929172706634e+002, 0., 0., 1.);
-	cv::Matx<double, 5, 1> distortion_coefficients(
-		2.0963551753568421e-001, -1.4796846132520820e+000, 0., 0., 2.7677879392937270e+000);
+	cv::createTrackbar("H. Lower", "Blob", &(BlobLower[0]), 255);
+	cv::createTrackbar("H. Upper", "Blob", &(BlobUpper[0]), 255);
+	cv::createTrackbar("S. Lower", "Blob", &(BlobLower[1]), 255);
+	cv::createTrackbar("S. Upper", "Blob", &(BlobUpper[1]), 255);
+	cv::createTrackbar("V. Lower", "Blob", &(BlobLower[2]), 255);
+	cv::createTrackbar("V. Upper", "Blob", &(BlobUpper[2]), 255);
 
 	cv::Point textOrg;
-	std::list<cv::Vec3d> qRo;
+	DataSet locations;
 
 	for (; true;) {
+		int key = 0xff & cv::waitKey(50);
+		if ((key & 255) == 27) break;
+
 		capture >> Im;
 		if (Im.empty()) {
 			std::cout << " Error reading from camera" << std::endl;
@@ -71,127 +157,66 @@ int main(int argc, char** argv)
 		//morphological opening (remove small objects from the foreground)
 		cv::erode(BlobIm, BlobIm, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3)));
 		cv::dilate(BlobIm, BlobIm, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3)));
-
 		//morphological closing (fill small holes in the foreground)
 		cv::dilate(BlobIm, BlobIm, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3)));
 		cv::erode(BlobIm, BlobIm, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3)));
 		cv::medianBlur(BlobIm, BlobIm, 21);
 		*/
+
 		//Extract Contours
 		cv::Mat bw;
 		BlobIm.convertTo(bw, CV_8UC1);
 		std::vector<std::vector<cv::Point>> contours;
-		//std::vector<cv::Vec4i> hierarchy;
 
 		cv::findContours(bw, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
 
 		if (contours.size() > 0) {
-			// Find Largest Contour
-			size_t maxid = contours.size();
-			size_t secid = contours.size();
-			double sim1 = 100.0, sim2 = 100.0;
-			for (size_t i = 0; i < contours.size(); i++)
+			std::vector<cv::Point> target;
+			double sim1 = 1000.0;
+			for (std::vector<cv::Point> cont : contours)
 			{
-				if (cv::contourArea(contours[i]) > 256) {
-					double similarity = cv::matchShapes(stencil, contours[i], CV_CONTOURS_MATCH_I3, 1);
+				// Only process cont if it is big enough, otherwise it's either too far or just a noise
+				if (cv::contourArea(cont) > 1000) {
+					double similarity = cv::matchShapes(stencil, cont, CV_CONTOURS_MATCH_I3, 1);
 					if (similarity < sim1)
 					{
-						secid = maxid;
-						maxid = i;
+						target = cont;
 						sim1 = similarity;
 					}
-					else if (similarity < sim2) {
-						secid = i;
-						sim2 = similarity;
-					}
-
 				}
 			}
-			for (std::vector<std::vector<cv::Point>>::iterator it = contours.begin(); it != contours.end(); ++it) {
-				cv::polylines(Im, *it, true, cv::Scalar(255, 0, 0));
+
+			/*
+			for (std::vector<cv::Point> it : contours) {
+				cv::polylines(Im, it, true, cv::Scalar(255, 0, 0));
 			}
-			//Extract corner points assuming the two blobs are a credit card
-			std::vector<cv::Point> hull;
-			if (maxid < contours.size()) {
-				hull.push_back(cv::Point(bw.cols, bw.rows));	// North-West
-				hull.push_back(cv::Point(0, bw.rows));			// North-East
-				hull.push_back(cv::Point(0, 0));				// South-East
-				hull.push_back(cv::Point(bw.cols, 0));			// South-West
-				for (size_t id = maxid; id == maxid; id = secid) {
-					for (size_t i = 0; i < contours[id].size(); ++i) {
-						if (hull[0].x + hull[0].y > contours[id][i].x + contours[id][i].y) {
-							hull[0].x = contours[id][i].x;
-							hull[0].y = contours[id][i].y;
-						}
-						if (bw.cols - hull[1].x + hull[1].y > bw.cols - contours[id][i].x + contours[id][i].y) {
-							hull[1].x = contours[id][i].x;
-							hull[1].y = contours[id][i].y;
-						}
-						if (hull[2].x + hull[2].y < contours[id][i].x + contours[id][i].y) {
-							hull[2].x = contours[id][i].x;
-							hull[2].y = contours[id][i].y;
-						}
-						if (hull[3].x + bw.rows - hull[3].y > contours[id][i].x + bw.rows - contours[id][i].y) {
-							hull[3].x = contours[id][i].x;
-							hull[3].y = contours[id][i].y;
-						}
-					}
-				}
-				cv::polylines(Im, hull, true, cv::Scalar(0, 255, 0));
+			std::cout << "Similarity " << sim1 << std::endl;
+			*/
 
-				std::vector<cv::Point2d> imagePoints(hull.size());
-				for (size_t i = 0; i < hull.size(); ++i) {
-					imagePoints[i] = hull[i];
-				}
+			if (target.size() > 0 && sim1 < 2.0) {
+				cv::polylines(Im, target, true, cv::Scalar(0, 200, 255),4);
+				cv::Vec3d cameralocation = CalculateLocation(target);
 
-				cv::Vec3d rvec, tvec;
-				cv::solvePnP(objectPoints, imagePoints, camera_matrix, distortion_coefficients, rvec, tvec, false, CV_EPNP);
-				cv::Matx33d Rmat;
-				cv::Rodrigues(rvec, Rmat);
-				cv::Vec3d cameralocation;
-				cameralocation = -(Rmat.t() * tvec);
-				qRo.push_front(cameralocation);
-				if (qRo.size()>50) {
-					qRo.pop_back();
-				}
-				if (qRo.size()>2) {
-					std::list<double> ordX, ordY, ordZ;
-					std::list<double>::iterator it;
-					size_t i;
+				// Store calculations in a queue but use a list instead so we can iterate
+				locations.push_front(cameralocation);
+				if (locations.size()>50) locations.pop_back();
 
-					for (std::list<cv::Vec3d>::iterator dp = qRo.begin(); dp != qRo.end(); ++dp) {
-						ordX.push_back((*dp)[0]);
-						ordY.push_back((*dp)[1]);
-						ordZ.push_back((*dp)[2]);
-					}
-					ordX.sort();
-					ordY.sort();
-					ordZ.sort();
-					i = ordX.size() / 2; it = ordX.begin();
-					while (i > 0 && it != ordX.end()) ++it, --i;
-					cameralocation[0] = *it;
-					i = ordY.size() / 2; it = ordY.begin();
-					while (i > 0 && it != ordY.end()) ++it, --i;
-					cameralocation[1] = *it;
-					i = ordZ.size() / 2; it = ordZ.begin();
-					while (i > 0 && it != ordZ.end()) ++it, --i;
-					cameralocation[2] = *it;
+				// When we collect enough data get the median value for each coordinate
+				// Median rather than average because median tolerate noise better
+				if (locations.size()>2) {
+					// Replace camera location with improved values
+					cameralocation = locations.GetMedian();
 				}
 				double Ro = sqrtl(cameralocation.dot(cameralocation));
 				std::ostringstream oss;
-				oss << "Camera is at: "<< Ro <<", XYZ: "<< cameralocation << std::endl;
-				cv::putText(Im, oss.str(), textOrg, 1, 1, cv::Scalar(255,0,255));
+				oss << cameralocation << " " << Ro << std::endl;
+				cv::putText(Im, oss.str(), textOrg, 1, 2, cv::Scalar(0, 200,255), 2);
 			}
 		}
 
 
 		cv::imshow("Image", Im);
 		cv::imshow("Blob", BlobIm);
-//		cv::imshow("Test", hsvIm);
-		int key = 0xff & cv::waitKey(50);
-
-		if ((key & 255) == 27) break;
-
 	}
 	return 0;
 }
